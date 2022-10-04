e638e11547831c1bc578c54dd739ee30b03e67f1
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-10-04 10:22:13 -0300

    :wrench: chore: create basic files to RESTapi architeture

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import router from "./routes";
 import handleErrorMiddleware from "./middlewares/handleErrorsMiddleware";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
 app.use(handleErrorMiddleware);
 
 export default app;
