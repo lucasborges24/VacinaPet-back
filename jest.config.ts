f52c3c88625a0c1f79268d3161238b08d196ec03
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-10-04 09:39:09 -0300

    :wrench: chore: create jest config with basic configs

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export default {
+  preset: "ts-jest",
+  clearMocks: true,
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  collectCoverage: true,
+  // setupFilesAfterEnv: ["./jest.setup.ts"],
+//   coveragePathIgnorePatterns: [
+//     '<rootDir>/src/app.ts',
+//     '<rootDir>/src/database.ts',
+//     '<rootDir>/src/utils',
+//     '<rootDir>/src/middlewares',
+//     '<rootDir>/src/controllers',
+//     '<rootDir>/src/repositories',
+//     '<rootDir>/src/routers',
+//     '<rootDir>/src/schemas',
+//     '<rootDir>/tests/factories',
+// ]
+};
