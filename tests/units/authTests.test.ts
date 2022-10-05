007114955f7f3a75ce41fb6ba6b75b87e64493e3
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-10-05 11:07:34 -0300

    :heavy_check_mark: test: add unit test to Service SignIn function

--- tests/units/authTests.test.ts
+++ tests/units/authTests.test.ts
@@ -64,3 +64,47 @@ describe("Test signUp function", () => {
     expect(authRepository.getUserByEmail).toBeCalledTimes(1);
   });
 });
+
+describe("Test signIn function", () => {
+  it("Should return an unauthorized error when email doesn't exist", async () => {
+    const body = signInFactory();
+
+    jest.spyOn(bcrypt, "compareSync");
+    jest.spyOn(authRepository, "getUserByEmail").mockResolvedValueOnce(null);
+
+    const promise = authService.signIn(body);
+
+    expect(promise).rejects.toEqual(unauthorizedError("Invalid data!"));
+    expect(bcrypt.compareSync).not.toBeCalled();
+  });
+
+  it("Should return an unauthorized error when body's password is not equal db password", async () => {
+    const body = signInFactory();
+    const user = createdUserFactory();
+
+    jest.spyOn(bcrypt, "compareSync").mockImplementationOnce(() => false);
+    jest.spyOn(authRepository, "getUserByEmail").mockResolvedValueOnce(user);
+    jest.spyOn(jwt, "sign");
+
+    const promise = authService.signIn(body);
+
+    expect(promise).rejects.toEqual(unauthorizedError("Invalid data!"));
+    expect(jwt.sign).not.toBeCalled();
+  });
+
+  it("should return a token", async () => {
+    const body = signInFactory();
+    const user = createdUserFactory();
+    const token = "token charmoso";
+
+    jest.spyOn(bcrypt, "compareSync").mockImplementationOnce(() => true);
+    jest.spyOn(authRepository, "getUserByEmail").mockResolvedValueOnce(user);
+    jest.spyOn(jwt, "sign").mockImplementationOnce(() => token);
+
+    const result = await authService.signIn(body);
+    console.log(result);
+    
+    expect(result).toEqual({ token });
+    expect(jwt.sign).toBeCalledTimes(1);
+  });
+});
